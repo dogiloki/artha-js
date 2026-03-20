30693662014d6947f3a90b928f40010e11b380e6
dogiloki <wolftri2002@gmail.com>
2026-03-20 16:26:26 -0600

    sass implementation for style development and live server

---
+++ src/components/artha-message.js
@@ -0,0 +1,10 @@
+import Config from "./Config.js";
+import Util from "./Util.js";
+
+export default class ArthaMessage extends HTMLElement{
+
+    connectedCallback(){
+        
+    }
+
+}
--- src/core/TaskQueue.js
+++ src/core/TaskQueue.js
@@ -1,3 +1,48 @@
+import Config from "./Config.js";
+import Util from "./Util.js";
+
 export default class TaskQueue{
     
+    static INSTNACE=null;
+
+    static singleton(){
+        if(!this.INSTNACE){
+            this.INSTNACE=new TaskQueue();
+        }
+        return this.INSTNACE;
+    }
+
+    constructor(){
+        this.queues=new Map();
+    }
+
+    // Crear una nueva tarea
+    loadTask(id,title,callback,options={}){
+        if(typeof options!=='object'){
+            options={close:options};
+        }
+    }
+
+}
+
+class TaskQueueItem{
+
+    constructor(id,title,callback,options){
+        this.id=id;
+        this.callback=callback;
+        options={...Config.get("task_queue"),...options};
+        const {
+            title,
+            close,
+            message
+        }=options;
+        this.title=title;
+        this.message_element=options.message instanceof HTMLElement?options.message:document.querySelector("#"+options.message);
+        this.resolve_callback=null;
+        this.reject_callback=null;
+        this.finalize=false;
+        this.status="pending";
+        Util.modal(this.message_element,this.title,);
+    }
+
 }
